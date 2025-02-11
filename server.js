8de2b67f136a6b00edadeac6b88c25390feb414f
Code-Zzone <mahmoudabuelsaoudhamed@gmail.com>
2025-02-11 03:06:58 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + "/public")); // مجلد الملفات الثابتة
+
+io.on("connection", (socket) => {
+    console.log("📞 مستخدم متصل");
+
+    socket.on("offer", (offer) => {
+        socket.broadcast.emit("offer", offer);
+    });
+
+    socket.on("answer", (answer) => {
+        socket.broadcast.emit("answer", answer);
+    });
+
+    socket.on("ice-candidate", (candidate) => {
+        socket.broadcast.emit("ice-candidate", candidate);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("❌ مستخدم انفصل");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("🚀 السيرفر يعمل على http://localhost:3000");
+});
